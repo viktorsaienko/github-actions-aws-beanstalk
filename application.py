47c6f4ef50c298edd7ac3ec82a1051cf76a8ec9c
z53rb65y <z53rb65y@cloud-mentor.local>
2026-09-22 10:29:50 +0000

    Update application.py for verification

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Hello from z53rb65y Elastic Beanstalk CI-CD"
+    return "Hello from z53rb65y Elastic Beanstalk CI-CD verification-202609221009507514"
